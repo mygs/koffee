2b2057f59418ff0885a9f5b0557b3531022fae49
msaito <saito.milton@gmail.com>
2017-06-04 07:07:28 -0300

    running python 2.7 and 3.4

--- plotting.py
+++ plotting.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python
+import os
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import platform
 
 def autolabel(rects):
     for rect in rects:
         height = rect.get_height()
         width = rect.get_width()
-        ax.text(1.08*width,rect.get_y() + height/2.,
+        ax.text(width+0.08,rect.get_y() + height/2.,
                 "${:.2f}".format(width),
                 ha='center', va='center')
-
-
+print ('python version '+platform.python_version())
 fig, ax = plt.subplots()
-
-df = pd.read_csv('./data/capsule-prices-20170520.csv', delimiter=';')
-df_filtered = df[(df.date == 20170520) & (df.name == 'Ristretto')].sort_values(['brl'], ascending=False)
-y_pos = np.arange(len(df_filtered['country']))
-rec = ax.barh(y_pos, df_filtered['brl'].as_matrix(), linewidth=0, align='center', color='#72D776')
+os.chdir(sys.path[0]) # change working directory to script directory
+df = pd.read_csv('./data/capsule-prices-20170603.csv', delimiter=';')
+df_filtered = df[(df.date == 20170603) & (df.name == 'Ristretto')].sort_values(['brl'], ascending=False)
+countries = df_filtered['country'].as_matrix()
+n_countries = len(countries)
+y_pos = np.arange(n_countries)
+prices = df_filtered['brl'].as_matrix()
+rec = ax.barh(y_pos, prices, linewidth=0, align='center', color='#72D776')
+xmin = prices.min() - (prices.max() - prices.min())*.08
+xmax = prices.max() + (prices.max() - prices.min())*.16
+ax.set(xlim=[xmin,xmax])
 ax.set_yticks(y_pos)
-ax.set_yticklabels(df_filtered['country'].as_matrix())
+ax.set(ylim=[-1,n_countries+1])
+ax.set_yticklabels(countries)
 ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel('Ristretto Capsule Price')
 ax.set_title('How much Ristretto capsule cost around the world?')
-
 autolabel(rec)
-
 plt.show()
--- pricesCSV.py
+++ pricesCSV.py
@@ -23,7 +23,10 @@ def getAllRecordsFromGoogleSheets(filename, sheetname):
 
 def getNespressoBlockConfigJson( _url ):
     PATTERN='var blockConfig =(.*),\n'
-    class AppURLopener(urllib.FancyURLopener):version = "Mozilla/5.0"
+    if sys.version_info[0] == 2:
+        class AppURLopener(urllib.FancyURLopener):version = "Mozilla/5.0"
+    else:
+        class AppURLopener(urllib.request.FancyURLopener):version = "Mozilla/5.0"
     req = AppURLopener().open(_url)
     webpage = req.read().decode('utf-8')
     blockConfigStr = re.search(PATTERN, webpage)
@@ -49,10 +52,10 @@ def currencyConvert (externalPrice, fx):
 def saveBlockConfig( writer, date, country, fx, json ):
     for type in json['groups']:
         for coffees in type['products']:
-            id = coffees['id'].encode('utf-8')
-            name = coffees['name'].encode('utf-8')
+            id = coffees['id']#.encode('utf-8')
+            name = coffees['name']#.encode('utf-8')
             price = coffees['price']
-            iconHref = coffees['iconHref'].encode('utf-8')
+            iconHref = coffees['iconHref']#.encode('utf-8')
             salesMultiple = coffees['addToCartButton']['salesMultiple']
             preco = currencyConvert(price, fx)
             if salesMultiple == 10 and preco != None:
@@ -62,11 +65,11 @@ def saveBlockConfig( writer, date, country, fx, json ):
 def saveQuickCapsules( writer, date, country, fx, json ):
     for range in json['capsuleRange']:
         for list in range['capsuleList']:
-            name = list['name'].encode('utf-8')
+            name = list['name']#.encode('utf-8')
             price = list['priceValue']
-            id = list['code'].encode('utf-8')
-            type = list['type'].encode('utf-8')
-            iconHref = list['mediaQuickOrder']['url'].encode('utf-8')
+            id = list['code']#.encode('utf-8')
+            type = list['type']#.encode('utf-8')
+            iconHref = list['mediaQuickOrder']['url']#.encode('utf-8')
             salesMultiple = list['salesMultiple']
             preco = currencyConvert(price, fx)
             if salesMultiple == 10 and preco != None:
@@ -80,7 +83,7 @@ def saveQuickCapsules( writer, date, country, fx, json ):
 startGlobal = time.time()
 print ('python version '+platform.python_version())
 
-timestamp = datetime.date.today().strftime("%Y%m%d")
+timestamp = datetime.date.today().strftime("%Y%m%d-X")
 os.chdir(sys.path[0]) # change working directory to script directory
 csv_file='./data/capsule-prices-'+timestamp+'.csv'
 
@@ -89,24 +92,22 @@ try:
     writer = csv.writer(f, delimiter=';')
     writer.writerow( ('date','country','id','name','localprice','brl','iconHref') )
     for locale in getAllRecordsFromGoogleSheets('nespresso', 'locale'):
-        if locale['status'] == 'to_test':
+        if locale['status'] == 'ok':
             fx = locale['fx']
             country = locale['country']
             url = locale['capsules_url']
             if locale['extract_strategy'] == 'blockConfig':
                 start = time.time()
-                print('Processing blockConfig from '+ country),
                 saveBlockConfig(writer, timestamp, country, fx, getNespressoBlockConfigJson(url) )
                 end = time.time()
-                print('. Took '+ str(end - start) + ' seconds!')
+                print('Processed Nespresso(blockConfig) from '+ country+'. Took '+ str(int(end - start)) + ' seconds!')
             if locale['extract_strategy'] == 'quickCapsules':
                 start = time.time()
-                print('Processing quickCapsules from '+ country),
                 saveQuickCapsules(writer, timestamp, country, fx, getNespressoQuickCapsulesJson(url) )
                 end = time.time()
-                print('. Took '+ str(end - start) + ' seconds!')
+                print('\rProcessed Nespresso(quickCapsules) from '+ country+'. Took '+ str(int(end - start)) + ' seconds!')
 finally:
     f.close()
 endGlobal = time.time()
 globalMinutes, globalSeconds = divmod(endGlobal - startGlobal , 60)
-print('All process took '+ str(globalMinutes) + ' minutes and '+ str(globalSeconds) + ' seconds!')
+print('\rAll process took '+ str(globalMinutes) + ' minutes and '+ str(int(globalSeconds)) + ' seconds!')
