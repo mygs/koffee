8659ed21528daad8d72fa2ecefbb29f295094d38
mygs <saito.milton@gmail.com>
2017-06-15 21:11:50 -0300

    adding background img

--- plotting.py
+++ plotting.py
@@ -3,11 +3,12 @@ import os
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 import numpy as np
 import platform
 
 capsule = 'Ristretto'
-date = 20170610
+date = 20170615
 
 def autolabel(rects):
     for rect in rects:
@@ -17,23 +18,30 @@ def autolabel(rects):
                 "${:.2f}".format(width),
                 ha='center', va='center')
 print ('python version '+platform.python_version())
-fig, ax = plt.subplots()
+rcParams.update({'figure.autolayout': True})
 os.chdir(sys.path[0]) # change working directory to script directory
+fig, ax = plt.subplots()
 df = pd.read_csv('./data/capsule-prices-'+str(date)+'.csv', delimiter=';')
-df_filtered = df[(df.date == date) & (df.name == capsule)& (df.fx == 'BRL')].sort_values(['price'], ascending=False)
+df_filtered = df[(df.date == date)&(df.name == capsule)&(df.fx == 'BRL')].sort_values(['price'], ascending=True)
+#print(df_filtered)
 countries = df_filtered['country'].as_matrix()
 n_countries = len(countries)
 y_pos = np.arange(n_countries)
 prices = df_filtered['price'].as_matrix()
-rec = ax.barh(y_pos, prices, linewidth=0, align='center', color='#72D776')
+rec = ax.barh(y_pos, prices, linewidth=0, align='center', color='#72D776',zorder=3)
+
 xmin = prices.min() - (prices.max() - prices.min())*.08
 xmax = prices.max() + (prices.max() - prices.min())*.16
-ax.set(xlim=[xmin,xmax])
+img = plt.imread("img/background.png")
+#[left, right, bottom, top]
+ax.imshow(img, aspect='auto', zorder=0, extent=[xmin, xmax,-1, n_countries])
+#ax.set(xlim=[xmin,xmax])
+#ax.set(ylim=[-1,n_countries])
 ax.set_yticks(y_pos)
-ax.set(ylim=[-1,n_countries+1])
 ax.set_yticklabels(countries)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Ristretto Capsule Price')
-ax.set_title('How much Ristretto capsule cost around the world?')
+#ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel(capsule+' Capsule Price')
+ax.set_title('How much '+capsule+' capsule cost around the world?')
 autolabel(rec)
+
 plt.show()
